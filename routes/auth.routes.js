0ae783025ea9bc87c63631a3a39a906543f3fa09
Julia Ganina <ganina.julia2@gmail.com>
2023-02-04 15:08:45 +0000

    styling nearly done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -63,7 +63,7 @@ router.post("/login", (req, res) => {
   User.findOne({ username }).then((user) => {
     if (!user) {
       res.render("auth/login", {
-        errorMessage: "User not found. No account associated with email",
+        errorMessage: "User not found.",
       });
     } else if (bcrypt.compareSync(password, user.password)) {
       req.session.currentUser = user.toObject();
